27c91087f11b16940d473fdaac853c1d0927f57f
Roopesh Krishnan <roopeshkrishnanme@gmail.com>
2025-07-31 17:03:00 +0000

    did this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User, setPersistence, browserLocalPersistence } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,14 +14,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
-auth.useDeviceLanguage();
-setPersistence(auth, browserLocalPersistence);
-
 const provider = new GoogleAuthProvider();
+
+// This ensures the authentication request is explicitly associated with your project's domain.
 provider.setCustomParameters({
-    'login_hint': 'user@example.com'
+  authDomain: firebaseConfig.authDomain
 });
 
 
